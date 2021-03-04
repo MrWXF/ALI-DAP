d2c9f8e2cc6d2ca3ab9688cfc4022894665960c8
wzqcc <1249383313@qq.com>
2021-03-04 08:18:42 +0800

    初始文件

---
+++ ALI-DAP-NACOS/src/main/java/com/ali/wzq/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.ali.wzq;
+
+/**
+ * 类说明
+ *
+ * @Package com.ali.wzq
+ * @Author wuzhiqing
+ * @className ALI-DAP
+ * @Version v1.0.0
+ * @createData 2021/3/3  22:40
+ */
+public class NacosApplication {
+}
